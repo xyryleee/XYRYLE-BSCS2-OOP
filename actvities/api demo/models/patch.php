b2946e1991630fb35e135ccbbb9a33e8901354a6
xy <catorxyrylee@gmail.com>
2025-10-30 09:48:57 +0800

    Created Patch class for basic API testing

---
+++ actvities/api demo/models/patch.php
@@ -0,0 +1,15 @@
+<?php
+
+class Patch{
+    public function patchStudents(){
+        return "PATCHING STUDENT....";
+    }
+    public function patchFaculty(){
+        return "PATCHING FACULTY....";
+    }
+    public function logIn(){
+        return "LOGGING IN.....";
+    }
+}
+
+?>
