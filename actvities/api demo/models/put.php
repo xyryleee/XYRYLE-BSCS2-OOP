0d6af8330e876f42b0cc764ed513bff4d80485e4
xy <catorxyrylee@gmail.com>
2025-10-30 09:49:43 +0800

    Create put.php

---
+++ actvities/api demo/models/put.php
@@ -0,0 +1,15 @@
+<?php
+
+class Put{
+    public function putStudents(){
+        return "PUTTING STUDENTS....";
+    }
+    public function putFaculty(){
+        return "PUTTING FACULTY....";
+    }
+    public function logIn(){
+        return "LOGGING IN.....";
+    }
+}
+
+?>
