3cc4a611cd318f81b20665a51dd9ecb4a25e25b4
xy <catorxyrylee@gmail.com>
2025-10-30 09:49:21 +0800

    Created Post class for basic API testing

---
+++ actvities/api demo/models/post.php
@@ -0,0 +1,15 @@
+<?php
+
+class Post{
+    public function addStudents(){
+        return "ADDING STUDENTS....";
+    }
+    public function addFaculty(){
+        return "ADDING FACULTY....";
+    }
+    public function logIn(){
+        return "LOGGING IN.....";
+    }
+}
+
+?>
