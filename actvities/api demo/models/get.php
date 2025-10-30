bb46aa6a80ebb31d59b1ce2eff8461ddbee47de9
xy <catorxyrylee@gmail.com>
2025-10-30 09:47:42 +0800

    Created Get class for basic API testing

---
+++ actvities/api demo/models/get.php
@@ -0,0 +1,15 @@
+<?php
+
+class Get{
+    public function getStudents(){
+        return "GETTING STUDENTS....";
+    }
+    public function getFaculty(){
+        return "GETTING FACULTY....";
+    }
+    public function logIn(){
+        return "LOGGING IN.....";
+    }
+}
+
+?>
