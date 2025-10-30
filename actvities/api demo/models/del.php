515e45f660f4f569aa532c050da7731164b3567e
xy <catorxyrylee@gmail.com>
2025-10-30 09:47:12 +0800

    Created Delete class for basic API testing

---
+++ actvities/api demo/models/del.php
@@ -0,0 +1,15 @@
+<?php
+
+class Delete{
+    public function delStudents(){
+        return "DELETING STUDENT....";
+    }
+    public function delFaculty(){
+        return "DELETING FACULTY....";
+    }
+    public function logIn(){
+        return "LOGGING IN.....";
+    }
+}
+
+?>
